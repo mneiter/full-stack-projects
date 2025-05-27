bcbf3924b84cfdd9b6bdae8a50c8c70de7bbc956
merukimon <mneiter@gmail.com>
2025-05-27 10:49:41 +0300

    Adds CORS middleware to enhance API security
    Implements CORS middleware in the FastAPI application to control access from specified origins.
    
    This change allows requests from designated frontend environments while maintaining security by restricting access from unauthorized sources.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,28 @@
 import os
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 
 root_path = os.getenv("APP_ROOT_PATH", "")
 app = FastAPI(root_path=root_path)
+
+origins = [
+    "http://localhost:3000",
+    "http://dev.localhost",
+    "https://dev.localhost",
+    "http://staging.localhost",
+    "https://staging.localhost",
+    "http://prod.localhost",
+    "https://prod.localhost",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/ping")
 def ping():
